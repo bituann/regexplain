986fb0ac6a437f8acbbbcc2cd6bad2991283917b
bituan <113036994+bituann@users.noreply.github.com>
2025-11-03 01:29:34 +0100

    Add POST endpoint to access AI agent service

--- src/main/java/com/bituan/regexplain/controller/RegExPlainController.java
+++ src/main/java/com/bituan/regexplain/controller/RegExPlainController.java
@@ -2,6 +2,9 @@ package com.bituan.regexplain.controller;
 
 import com.bituan.regexplain.service.RegExPlainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +14,11 @@ public class RegExPlainController {
     @Autowired
     RegExPlainController (RegExPlainService regexplainService) {
         this.regexplainService = regexplainService;
+    }
+
+    @PostMapping("/explain")
+    public ResponseEntity<String> explainRegex (@RequestParam(value = "regex") String regex) {
+        String response = regexplainService.generateResponse(regex);
+        return ResponseEntity.ok(response);
     }
 }
