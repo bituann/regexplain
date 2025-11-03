cdb23052679aeb5d7cde02795e6b5326dde77cb1
bituan <113036994+bituann@users.noreply.github.com>
2025-11-03 04:13:14 +0100

    Add model for result property

---
+++ src/main/java/com/bituan/regexplain/model/Result.java
@@ -0,0 +1,69 @@
+package com.bituan.regexplain.model;
+
+import java.util.List;
+
+public class Result {
+    private String id;
+    private String contextId;
+    private TaskStatus status;
+    private Artifact artifacts;
+    private List<HistoryMessage> history;
+    private String kind;
+
+    public Result(String id, String contextId, TaskStatus status, Artifact artifacts, List<HistoryMessage> history) {
+        this.id = id;
+        this.contextId = contextId;
+        this.status = status;
+        this.artifacts = artifacts;
+        this.history = history;
+        this.kind = "task";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContextId() {
+        return contextId;
+    }
+
+    public void setContextId(String contextId) {
+        this.contextId = contextId;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public Artifact getArtifacts() {
+        return artifacts;
+    }
+
+    public void setArtifacts(Artifact artifacts) {
+        this.artifacts = artifacts;
+    }
+
+    public List<HistoryMessage> getHistory() {
+        return history;
+    }
+
+    public void setHistory(List<HistoryMessage> history) {
+        this.history = history;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+}
