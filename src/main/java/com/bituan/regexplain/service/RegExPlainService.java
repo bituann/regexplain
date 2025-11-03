1ad0123947e5384727aba973a43dba5dabdac350
bituan <113036994+bituann@users.noreply.github.com>
2025-11-03 18:53:37 +0100

    Add tool annotation to generateResponse service

--- src/main/java/com/bituan/regexplain/service/RegExPlainService.java
+++ src/main/java/com/bituan/regexplain/service/RegExPlainService.java
@@ -1,6 +1,7 @@
 package com.bituan.regexplain.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class RegExPlainService {
         this.chatClient = chatClientBuilder.build();
     }
 
+    @Tool(name = "regexplain", description = "An agent that explains what type of string a regex pattern matches")
     public String generateResponse(String regex) {
         return chatClient
                 .prompt("Give me a simple explanation of the type of string matched by this regex pattern: %s. No validating statements from you. Just straight to the point".formatted(regex))
