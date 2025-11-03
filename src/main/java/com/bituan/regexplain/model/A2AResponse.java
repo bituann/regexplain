8b4c2322732f97ff23508a131f51630d2cd502d8
bituan <113036994+bituann@users.noreply.github.com>
2025-11-03 03:57:57 +0100

    Add model for a2a response

---
+++ src/main/java/com/bituan/regexplain/model/A2AResponse.java
@@ -0,0 +1,43 @@
+package com.bituan.regexplain.model;
+
+public class A2AResponse {
+    private final String jsonrpc;
+    private String id;
+    private Result result;
+    private CustomError error;
+
+    public A2AResponse(String id, Result result, CustomError error) {
+        this.jsonrpc = "2.0";
+        this.id = id;
+        this.result = result;
+        this.error = error;
+    }
+
+    public String getJsonrpc() {
+        return jsonrpc;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public CustomError getError() {
+        return error;
+    }
+
+    public void setError(CustomError error) {
+        this.error = error;
+    }
+}
