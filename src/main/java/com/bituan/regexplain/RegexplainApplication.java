5f17c19d240320d67ce02ee497a966cc8edb86fd
bituan <113036994+bituann@users.noreply.github.com>
2025-11-03 01:10:00 +0100

    Initial setup

---
+++ src/main/java/com/bituan/regexplain/RegexplainApplication.java
@@ -0,0 +1,13 @@
+package com.bituan.regexplain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegexplainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegexplainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bituan/regexplain/RegexplainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bituan.regexplain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegexplainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
