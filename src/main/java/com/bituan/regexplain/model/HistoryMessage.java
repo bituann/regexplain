4d066217eed2bcd8ec6a83d7bc295a820580156c
bituan <113036994+bituann@users.noreply.github.com>
2025-11-03 12:45:31 +0100

    Ignore null properties

--- src/main/java/com/bituan/regexplain/model/HistoryMessage.java
+++ src/main/java/com/bituan/regexplain/model/HistoryMessage.java
@@ -1,10 +1,12 @@
 package com.bituan.regexplain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class HistoryMessage {
     private String kind;
     private String role;
